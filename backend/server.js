ed2fb815af1dea4c0187a04b23a62c00c646db30
KomalMahajan0102 <komalmahajan235@gmail.com>
2025-06-08 12:49:59 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -236,9 +236,7 @@ app.post('/api/users', async (req, res) => {
   }
 });
 
-app.get("/",(req,res)=>{
-  res.send("server is running");
-})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
