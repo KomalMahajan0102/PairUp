8668f20950a43b2a2a4a5354d49ac58aa4544dcb
KomalMahajan0102 <127200045+KomalMahajan0102@users.noreply.github.com>
2025-06-09 21:40:57 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ function App() {
 
     try {
       if (formData.wantsToTalk) {
-        const response = await fetch('https://pairup-api-n4kw.onrender.com', {
+        const response = await fetch('https://pairup-csk-api.onrender.com', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
